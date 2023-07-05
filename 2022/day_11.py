d40c26d38422056f41ff9914b2bfca237c42bfd2
Eric Tran <eric.tran@crunchyroll.com>
2023-07-05 09:24:46 -0400

    merge

--- 2022/day_11.py
+++ 2022/day_11.py
@@ -9,16 +9,26 @@
 
 
 class Monkey:
-    def __init__(self):
-        self.items = []
-        self.inspection = None # function
+    def __init__(self, id, items, operation, test):
+        self.id = id
+        self.items = items
+        self.operation = None # function
         self.test = None # function
-        self.inpection_count = None
+        self.inspection_count = 0
+    
+    # inspects each item in the list
+    def inspect_items(self, worry):
+        for item in item:
+            self.operation(item)
+            yield worry
+            self.test(item)
+            self.inspection_count += 1
     
     def throw(self):
         pass
 
     def catch(self, item):
         self.append(item)
-
-
+    
+    def __str__(self):
+        return f'Monkey {self.id}'
