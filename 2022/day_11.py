60eebbe81ffd87ad5284c17dfabd89c892fbe2c8
Eric Tran <traneric94@gmail.com>
2023-07-05 08:51:29 -0400

    day 11 start

---
+++ 2022/day_11.py
@@ -0,0 +1,24 @@
+
+# make monkeys
+
+# loop through monkeys
+# monkey inspects
+# relief // 3
+# monkey test
+# monkey throw
+
+
+class Monkey:
+    def __init__(self):
+        self.items = []
+        self.inspection = None # function
+        self.test = None # function
+        self.inpection_count = None
+    
+    def throw(self):
+        pass
+
+    def catch(self, item):
+        self.append(item)
+
+
