409ff0b8a148952039d7ef89b954e79f1c1e3164
Eric Tran <traneric94@gmail.com>
2025-12-26 17:17:03 -0500

    day 2

--- 2025/day_1.py
+++ 2025/day_1.py
@@ -36,12 +36,37 @@ def day_2(turns):
     print("count", counting)
 
 
+def test(lines):
+    pos = 50
+    p1 = 0
+    p2 = 0
+
+    for line in lines:
+        d = line[0]
+        amt = int(line[1:])
+
+        for _ in range(amt):
+            if d == "L":
+                pos = (pos - 1 + 100) % 100
+            else:
+                pos = (pos + 1) % 100
+
+            if pos == 0:
+                p2 += 1
+
+        if pos == 0:
+            p1 += 1
+
+    print(p1)
+    print(p2)
+
+
 if __name__ == "__main__":
     with open("Day_1_input.txt") as f:
+        # print("test", test(f.readlines()))
         lines = list(map(int, f.read().replace("R", "").replace("L", "-").split()))
 
     print("Day 1, Part 1")
     day_1(lines)
     print("Day 1, Part 2")
     day_2(lines)
-    answer(lines)
